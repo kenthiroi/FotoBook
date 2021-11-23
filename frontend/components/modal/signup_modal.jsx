4a9f895bc5e7242cdfdc458bea8f9d073c726bda
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-11-22 16:19:54 -0800

    Preparing for initial Heroku push

---
+++ frontend/components/modal/signup_modal.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class SignUpModal extends React.Component{
+  render(){
+    return <div className="signup-modal">
+      <form>
+        <input type="text" placeholder="First name"/>
+        <input type="text" placeholder="Last name"/>
+        <input type="text" placeholder="Email"/>
+        <input type="password" placeholder="New password"/>
+        <input type="date"/>
+        <input type="radio" value="Male"/>
+        <input type="radio" value="Female"/>
+        <input type="radio" value="Custom"/>
+      </form>
+    </div>
+  }
+}
