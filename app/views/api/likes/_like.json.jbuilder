b997e129d932ad1893eee7ed7456f550b5e7644d
Kent Hiroi <khiroi@mail.sfsu.edu>
2022-01-14 21:22:27 -0800

    added relations and API pages

---
+++ app/views/api/likes/_like.json.jbuilder
@@ -0,0 +1,3 @@
+json.extract! like, :id, :user_id, :post_id
+json.first_name post.user.first_name
+json.last_name post.user.last_name
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :show]
     resources :posts, only: [:show, :create, :update, :destroy, :index]
+    resources :likes, only: [:create, :destroy]
     resource :session, only: [:create, :destroy]
   end
 
