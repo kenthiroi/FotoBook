09bc52c768da9d8bdbb20108edca8e5e60059781
Kent Hiroi <khiroi@mail.sfsu.edu>
2022-01-19 23:16:04 -0800

    like actions

---
+++ frontend/actions/like_actions.js
@@ -0,0 +1,38 @@
+import * as LikeAPIUtil from '../util/likes_api_util'
+
+export const RECEIVE_LIKE = "RECEIVE_LIKE"
+export const REMOVE_LIKE = "REMOVE_LIKE"
+
+export const receiveLike = like => ({
+  type: RECEIVE_LIKE,
+  like
+})
+
+export const removeLike = likeId =>  ({
+  type: REMOVE_LIKE,
+  likeId
+})
+
+export const createLike = like => dispatch => (
+  LikeAPIUtil.createLike(like).then(like => (
+    dispatch(receiveLike(like))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+)
+
+export const getLike = likeId => dispatch => (
+  LikeAPIUtil.getLike(likeId).then(like => (
+    dispatch(receiveLike(like))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+)
+
+export const deleteLike = likeId => dispatch => (
+  LikeAPIUtil.deleteLike(likeId).then(() => (
+    dispatch(removeLike(likeId))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+)
