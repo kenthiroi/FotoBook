3c7ebcf6e341b5468311f2de708747492531d29b
Kent Hiroi <khiroi@mail.sfsu.edu>
2022-01-21 23:32:39 -0800

    created like button component

---
+++ frontend/components/like/like_button.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { createLike, deleteLike } from "../../actions/like_actions";
+
+const mountStateToProps = (state) => {
+  return {
+
+  }
+}
+
+const mountDispatchToProps = (dispatch) => {
+  return {
+    createLike: (like) => createLike(like),
+    deleteLike: (likeId) => deleteLike(likeId)
+  }
+}
+
+
+class LikeButton extends React.Component{
+  
+}
+
+export default LikeButton
