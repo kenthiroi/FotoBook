1a9bd65f567d337ae07bf76c2f76ea7d5e28b28d
Kent Hiroi <khiroi@mail.sfsu.edu>
2022-05-20 18:52:14 -0700

    somewhat updating, some async issues

--- frontend/components/friends/friend_request_button.jsx
+++ frontend/components/friends/friend_request_button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux"
-import { createFriendRequest, getFriendRequests } from "../../actions/friend_request_actions";
+import { createFriendRequest, getFriendRequests, deleteFriendRequest } from "../../actions/friend_request_actions";
 
 const mSTP = (state) => {
   return {
@@ -12,6 +12,7 @@ const mSTP = (state) => {
 const mDTP = (dispatch) => {
   return {
     createFriendRequest: (friendReq) => dispatch(createFriendRequest(friendReq)),
+    deleteFriendRequest: (friendReqId) => dispatch(deleteFriendRequest(friendReqId)),
     fetchFriendRequests: (userId) => dispatch(getFriendRequests(userId)),
   }
 }
@@ -19,21 +20,10 @@ const mDTP = (dispatch) => {
 class FriendRequestButton extends React.Component{
   constructor(props){
     super(props);
-    // console.log(this.props.friendReqs);
-    // let friendRequest = this.props.friendReqs.find(fr => fr.user_id === this.props.profileId) || this.props.friendReqs.find(fr => fr.receiver_id === this.props.profileId);
-    // if (!!friendRequest) {
-    //   this.state = {
-    //     requestMade: true,
-    //     request: friendRequest,
-    //   }
-    // } else {
-      this.state = {
-        requestMade: false,
-        request: undefined,
-      }
-    // }
-
-    // console.log(friendRequest);
+    this.state = {
+      requestMade: false,
+      request: undefined,
+    }
 
     this.handleFriendRequest = this.handleFriendRequest.bind(this);
   }
@@ -44,25 +34,36 @@ class FriendRequestButton extends React.Component{
 
   componentDidUpdate(){
     let friendRequest = Object.values(this.props.friendReqs);
-    let searchFR = friendRequest.find(fr => fr.receiver_id === this.props.profileId);
-    console.log(searchFR);
+    console.log(friendRequest);
+    let friendRequestResult = friendRequest.find(fr => fr.user_id === this.props.profileId) || friendRequest.find(fr => fr.receiver_id === parseInt(this.props.profileId));
+    if (!!friendRequestResult && !this.state.requestMade) {
+      this.setState({requestMade: true, request: friendRequestResult})
+    }
   }
 
-  handleFriendRequest(e){
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append('friend_request[sender_id]', this.props.userId);
-    formData.append('friend_request[receiver_id]', parseInt(this.props.profileId));
-
-    this.props.createFriendRequest(formData);
+  handleFriendRequest(params){
+    // e.preventDefault();
+    switch(params) {
+      case 'add':
+        const formData = new FormData();
+        formData.append('friend_request[sender_id]', this.props.userId);
+        formData.append('friend_request[receiver_id]', parseInt(this.props.profileId)); 
+        this.props.createFriendRequest(formData);
+        break;
+      case 'delete':
+        this.props.deleteFriendRequest(this.state.request.id);
+        this.setState({requestMade: false, request: undefined});
+        break;
+      }
+      console.log(this.state.request);
   }
 
   render(){
     return (
       <div className="friend-request-box">
       {!this.state.requestMade ? 
-        <input type="submit" value="Add Friend" onClick={this.handleFriendRequest}/>
-       : <></>
+        <input type="submit" value="Add Friend" onClick={() => this.handleFriendRequest('add')}/>
+       : <input type="submit" value="Delete Request" onClick={() => this.handleFriendRequest('delete')}/>
       //  Add button here to handle removing friend
       }
       </div>
