b1b07ce4ad5268e68b765acc46576d62ce2bae43
Kent Hiroi <khiroi@mail.sfsu.edu>
2022-06-20 20:55:57 -0700

    building out more profile components

--- frontend/components/user/profileAbout.jsx
+++ frontend/components/user/profileAbout.jsx
@@ -10,6 +10,8 @@ class UserProfileAbout extends React.Component{
     this.state = {
       displayedInfo: 'overview',
     }
+
+    this.handleSwitch = this.handleSwitch.bind(this);
   }
 
   handleSwitch(tabName){
@@ -22,12 +24,46 @@ class UserProfileAbout extends React.Component{
       case 'overview':
         aboutContents = <div></div>;
         break;
+      case 'profession':
+        aboutContents = <div></div>;
+        break;
+      case 'places':
+        aboutContents = <div></div>;
+        break;
+      case 'contacts':
+        aboutContents = <div></div>;
+        break;
+      case 'details':
+        aboutContents = <div></div>;
+        break;
+      default:
+        aboutContents = <div></div>;
+        break;
     }
     return (
       <div>
         <div id='about-title'>About</div>
         <div className='about-tabs'>
-          <div></div>
+          <div className={(this.state.displayedInfo === 'overview') ? 'active-about-button' : ''} 
+            onClick={() => this.handleSwitch('overview')}>
+            Overview
+          </div>
+          <div className={(this.state.displayedInfo === 'profession') ? 'active-about-button' : ''} 
+            onClick={() => this.handleSwitch('profession')}>
+            Work and education
+          </div>
+          <div className={(this.state.displayedInfo === 'places') ? 'active-about-button' : ''} 
+            onClick={() => this.handleSwitch('places')}>
+            Places lived
+          </div>
+          <div className={(this.state.displayedInfo === 'contacts') ? 'active-about-button' : ''} 
+            onClick={() => this.handleSwitch('contacts')}>
+            Contact and basic info
+          </div>
+          <div className={(this.state.displayedInfo === 'details') ? 'active-about-button' : ''} 
+            onClick={() => this.handleSwitch('details')}>
+            Details about you
+          </div>
         </div>
         <div className='about-container'>
           {aboutContents}
