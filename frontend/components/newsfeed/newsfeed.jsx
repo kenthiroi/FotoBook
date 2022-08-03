e1c64416fb20cc9cbe6fbcee4efcd416605592fa
Kent Hiroi <khiroi@mail.sfsu.edu>
2022-08-02 21:56:13 -0700

    styled newsfeed post box

--- frontend/components/newsfeed/newsfeed.jsx
+++ frontend/components/newsfeed/newsfeed.jsx
@@ -3,8 +3,11 @@ import PostIndexContainer from "../posts/postIndexContainer";
 
 class Newsfeed extends React.Component {
   render(){
-    return <div className="newsfeed-box">
-        <div onClick={this.props.openModal}>Click Me to post</div>
+    return <div className="newsfeed-container">
+      <div className="create-post-container">
+        <img src={this.props.userImg}/>
+        <div onClick={this.props.openModal}>Whats on your mind, {this.props.userInfo.first_name}?</div>
+      </div>
         <PostIndexContainer/>
       </div>
   }
--- frontend/components/newsfeed/newsfeedContainer.jsx
+++ frontend/components/newsfeed/newsfeedContainer.jsx
@@ -3,10 +3,27 @@ import { openModal } from "../../actions/modal_actions"
 import { logout } from "../../actions/session_actions"
 import Newsfeed from "./newsfeed"
 
-const mapDispatchToProps = dispatch => {
+const mSTP = state => {
+  let userInfo = state.entities.user[state.session.id];
+  let userImg;
+  try {
+    // userImg = state.entities.posts[state.entities.user[state.session.id].profile_picture];
+    userImg = userInfo.photoUrl,
+    console
+  } catch (e) {
+    userImg = 'https://i.imgur.com/7x6fTDK.png';
+  }
+
+  return {
+    userInfo,
+    userImg,
+  }
+}
+
+const mDTP = dispatch => {
   return {
     openModal: () => dispatch(openModal({type:'createPost'}))
   }
 }
 
-export default connect(null, mapDispatchToProps)(Newsfeed)
+export default connect(mSTP, mDTP)(Newsfeed)
