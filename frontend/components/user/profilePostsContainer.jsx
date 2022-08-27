a19989485e707efe77ee120c6fdfc26622c86827
Kent Hiroi <khiroi@mail.sfsu.edu>
2022-08-26 20:45:37 -0700

    refactoring profile page

---
+++ frontend/components/user/profilePostsContainer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ProfilePostsContainer({userInfo, userImg, isOwner}){
+
+
+  return (
+    <div>
+      <div className="profile-left">
+
+      </div>
+      <div className="profile-right">
+        
+      </div>
+    </div>
+  )
+}
+
+export default ProfilePostsContainer
