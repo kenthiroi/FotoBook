1beac4826fad14d140cbd1d398638a46dc27d944
Kent Hiroi <khiroi@mail.sfsu.edu>
2022-10-10 21:31:46 -0700

    intro bio css

--- frontend/components/user/profileIntro.jsx
+++ frontend/components/user/profileIntro.jsx
@@ -107,7 +107,7 @@ class UserProfileIntro extends React.Component{
         {this.state.showForm ? 
           <div className="intro-form">
             <form>
-              <input onChange={this.updateState} placeholder="Describe who you are"></input>
+              <textarea onChange={this.updateState} placeholder="Describe who you are"></textarea>
               <button className="intro-save" onClick={this.handleSubmit}>Save</button>
               <button className="intro-cancel" onClick={this.closeForm}>Cancel</button>
             </form>
