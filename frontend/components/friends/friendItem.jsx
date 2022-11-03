2e464ee41b722f92d5a22db92337f63e3172f549
Kent Hiroi <khiroi@mail.sfsu.edu>
2022-11-02 23:08:47 -0700

    Added logic for user description

--- frontend/components/friends/friendItem.jsx
+++ frontend/components/friends/friendItem.jsx
@@ -4,6 +4,7 @@ import NameHover from '../posts/nameHover';
 function FriendItem({user}){
   
   let photoUrl;
+  let userDescription;
 
   if (!!user.photoUrl){
     photoUrl = user.photoUrl;
@@ -11,10 +12,28 @@ function FriendItem({user}){
     photoUrl = 'https://i.imgur.com/7x6fTDK.png';
   }
 
+  switch (user){
+    case !!user.work:
+      userDescription = user.work;
+      break;
+    case !!user.school:
+      userDescription = user.school;
+      break;
+    case !!user.hometown:
+      userDescription = user.hometown;
+      break;
+    default:
+      userDescription = '';
+  }
+
+
   return(
     <div className='friend-item'>
       <img src={user.photoUrl}/>
       <NameHover user={user}/>
+      <div>
+        {userDescription}
+      </div>
     </div>
   )
 }
