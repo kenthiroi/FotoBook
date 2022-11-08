9c76cf2128e470d95712b07106738d6587c46c47
Kent Hiroi <khiroi@mail.sfsu.edu>
2022-11-07 23:49:53 -0800

    Added logic to dynamically change profile page structure for mobile devices

--- frontend/components/user/profilePostsContainer.jsx
+++ frontend/components/user/profilePostsContainer.jsx
@@ -1,18 +1,36 @@
 import React from "react";
+import { useState } from "react";
 import ProfileIntro from "./profileIntro";
 import ProfileWall from "./profileWall";
+import useWindowDimensions from '../customHooks';
 
 function ProfilePostsContainer({userInfo, userImg, isOwner, handleSwitch}){
+  const { width } = useWindowDimensions();
+  const [mobileView, setMobileView] = useState(width >= 900);
 
+  console.log(width);
 
   return (
     <div className="profile-posts-container">
-      <div className="profile-left">
-        <ProfileIntro userInfo={userInfo} isOwner={isOwner} handleSwitch={handleSwitch}/>
-      </div>
-      <div className="profile-right">
-        <ProfileWall userInfo={userInfo} userImg={userImg} isOwner={isOwner}/>
-      </div>
+      {mobileView ? 
+        <>
+          <div className="profile-left">
+            <ProfileIntro userInfo={userInfo} isOwner={isOwner} handleSwitch={handleSwitch}/>
+          </div>
+          <div className="profile-right">
+            <ProfileWall userInfo={userInfo} userImg={userImg} isOwner={isOwner}/>
+          </div>
+        </>
+        : 
+        <>
+          <div className="profile-left">
+            <ProfileIntro userInfo={userInfo} isOwner={isOwner} handleSwitch={handleSwitch}/>
+          </div>
+          <div className="profile-right">
+            <ProfileWall userInfo={userInfo} userImg={userImg} isOwner={isOwner}/>
+          </div>
+        </>
+      }
     </div>
   )
 }
