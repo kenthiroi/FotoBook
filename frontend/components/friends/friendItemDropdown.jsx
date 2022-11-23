69c7f256cbbc508999badbab634f1c7b113ab722
Kent Hiroi <khiroi@mail.sfsu.edu>
2022-11-22 23:59:45 -0800

    FriendItemDropdown structure

--- frontend/components/friends/friendItemDropdown.jsx
+++ frontend/components/friends/friendItemDropdown.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { connect } from "react-redux";
+import { BsThreeDots } from 'react-icons/bs';
+import { IoTrashOutline } from 'react-icons/io5';
 import { deleteFriend } from '../../actions/friend_actions';
 
 const mSTP = (state) => {
@@ -17,16 +19,26 @@ const mDTP = (dispatch) => {
 function FriendItemDropdown({friendId, deleteFriend}){
   const [openDropdown, setOpenDropdown] = useState(false);
 
+  const 
+
   const removeFriend = () => {
     deleteFriend(friendId).then(() => {
       setOpenDropdown(false);
     })
   }
 
-  
-
   return(
     <div className='friend-item-dropdown'>
+      <button 
+        className="friend-item-option" 
+        onClick={openDropdown ? () => setOpenDropdown(false) : () => setOpenDropdown(true)} 
+        onBlur={() => setOpenDropdown(false)}>
+        <BsThreeDots/>
+      </button>
+      {openDropdown ? 
+        <div className="edit-container">
+          <button onMouseDown={() => removeFriend}><IoTrashOutline/>Delete Friend</button>
+        </div> : <></>}
     </div>
   )
 }
