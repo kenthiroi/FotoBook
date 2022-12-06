e1529d64c2cb9de02af4660d0ea607f6ac971053
Kent Hiroi <khiroi@mail.sfsu.edu>
2022-12-05 23:32:27 -0800

    hotfix for frienditem and profile-intro-button

--- frontend/components/friends/friendItem.jsx
+++ frontend/components/friends/friendItem.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import FriendItemDropdown from './friendItemDropdown';
 
 
-function FriendItem({user, friendNodeId}){
+function FriendItem({user, friendNodeId, isOwner}){
 
   
   let photoUrl;
