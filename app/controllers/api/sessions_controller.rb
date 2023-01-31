50587024479528de63e7d7a2431c2b73e0ecb1d8
Kent Hiroi <khiroi@mail.sfsu.edu>
2023-01-30 21:29:08 -0800

    Changed session controller error messages to match facebook

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,11 +1,15 @@
 class Api::SessionsController < ApplicationController
   def create
-    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
-    if @user
-      login(@user)
-      render "api/users/show"
+    if (User.find_by(email: params[:user][:email]))
+      @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+      if @user
+        login(@user)
+        render "api/users/show"
+      else
+        render json: ["Invalid login credentials"], status: 401
+      end
     else
-      render json: ["Invalid login credentials"], status: 401
+      render json: ["The email you entered isn’t connected to an account. Create a new account and log in."], status: 401
     end
   end
 
@@ -14,7 +18,7 @@ class Api::SessionsController < ApplicationController
     if @user
       logout
     else
-      render json: ["Not a valid action"], status: 404
+      render json: ["A user is not logged in."], status: 404
     end
   end
 end
--- frontend/reducers/errors_reducer/login_errors_reducer.js
+++ frontend/reducers/errors_reducer/login_errors_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_CURRENT_USER, RECEIVE_SESSION_ERRORS } from "../../actions/session_actions";
+import { RECEIVE_CURRENT_USER, RECEIVE_SESSION_ERRORS, CLEAR_ERRORS } from "../../actions/session_actions";
 
 
 export const loginErrorsReducer = (state=[], action) => {
