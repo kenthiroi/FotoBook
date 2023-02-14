e44b02ccf5ece7451fe5f0d5ff148fc73c2d947f
Kent Hiroi <khiroi@mail.sfsu.edu>
2023-02-13 23:57:40 -0800

    Attempting to fix auth

---
+++ frontend/components/componentRoutes.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import LoginPageContainer from './login/loginPageContainer';
+import Modal from './modal/modal';
+import NewsfeedContainer from './newsfeed/newsfeedContainer';
+import HeaderNav from './header/header'
+import UserProfile from './user/profile';
+import SideNav from './header/sideNav';
+
+const Home = ({ user }) => (
+  <>
+    <Modal/>
+    <Switch>
+      {user ? (
+        <Fragment>
+          <header>
+            <HeaderNav/>
+            <SideNav/>
+          </header>
+          {/* News Feed Component */}
+          <ProtectedRoute exact path='/newsfeed' component={NewsfeedContainer}/> 
+          <ProtectedRoute exact path='/profile/:userId' component={UserProfile} />
+          <ProtectedRoute exact path='/github' component={() => { 
+            window.location.href = 'https://github.com/kenthiroi'; 
+            return null;
+          }}/>
+          <ProtectedRoute exact path='/linkedin' component={() => { 
+            window.location.href = 'https://www.linkedin.com/in/kent-hiroi-381880103/'; 
+            return null;
+          }}/>
+        </Fragment>
+      ) : (
+        <AuthRoute exact path="/" component={LoginPageContainer}/>
+      )}
+    </Switch>
+  </>
+);
+
+const mSTP = (state) => ({
+  user: state.entities.users[state.session.id],
+});
+
+export default connect(mSTP, null)(Home);
