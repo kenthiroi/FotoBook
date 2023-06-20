2033241f39b51979dc48369e85e3a12c888e52f7
Kent Hiroi <khiroi@mail.sfsu.edu>
2023-06-19 21:53:25 -0700

    attempting to make seed data, hopefully will fix the db issues with the postgresql db on render

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Template:
+# User.create(first_name: '', last_name: '', email: '', password: '', birthday: '', gender: '', hometown: '', school: '', profile_picture: '', work: '', intro_bio: '', relationship: '');
+
+
+ActiveRecord::Base.transaction do
+  demo_user = User.create(first_name: '', last_name: '', email: '', password: '', birthday: '', gender: '', hometown: '', school: '', profile_picture: '', work: '', intro_bio: '', relationship: '');
