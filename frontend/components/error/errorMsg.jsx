fcc4b5f9b93c44ec3248296d7e3c82c133208876
Kent Hiroi <kenthiroi@gmail.com>
2023-08-10 23:55:40 -0700

    Adding error msg bubble component

---
+++ frontend/components/error/errorMsg.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const mSTP = (state) => {
+  return {
+
+  }
+}
+
+const mDTP = (dispatch) => {
+  return {
+    
+  }
+}
+
+function ErrorMsg({errorMsg}){
+  
+  return(
+    <div></div>
+  )
+}
+
+export default connect(mSTP, mDTP)(withRouter(SideNav));
