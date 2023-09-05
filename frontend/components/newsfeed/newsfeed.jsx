cb7dc986f51377502f30cbada5ddf8cb486793b2
Kent Hiroi <kenthiroi@gmail.com>
2023-09-05 00:00:05 -0700

    newsfeed index

--- frontend/components/newsfeed/newsfeed.jsx
+++ frontend/components/newsfeed/newsfeed.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import PostIndexContainer from "../posts/postIndexContainer";
 
-class Newsfeed extends React.Component {
-  render(){
-    return <div className="newsfeed-container">
-      <div className="create-post-container">
-        <div className="create-post-profile-pic">
-          <img src={this.props.userImg}/>
-        </div>
-        <div onClick={this.props.openModal}>Whats on your mind, {this.props.userInfo.first_name}?</div>
+function Newsfeed(){
+  return <div className="newsfeed-container">
+    <div className="create-post-container">
+      <div className="create-post-profile-pic">
+        <img src={this.props.userImg}/>
       </div>
-        <PostIndexContainer/>
-      </div>
-  }
+      <div onClick={this.props.openModal}>Whats on your mind, {this.props.userInfo.first_name}?</div>
+    </div>
+      <PostIndexContainer/>
+    </div>
 }
 
 export default Newsfeed
