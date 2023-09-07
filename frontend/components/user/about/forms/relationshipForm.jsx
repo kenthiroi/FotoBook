2c87903074473f5071ac0659852aa6fd374365f2
Kent Hiroi <kenthiroi@gmail.com>
2023-09-06 23:48:35 -0700

    relationship form

--- frontend/components/user/about/forms/relationshipForm.jsx
+++ frontend/components/user/about/forms/relationshipForm.jsx
@@ -10,57 +10,45 @@ const mDTP = dispatch => ({
   updateUserAbout: (user) => dispatch(updateUser(user)),
 })
 
-class RelationshipForm extends React.Component {
+function RelationshipForm(props){
+  const [value, setValue] = useState('');
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: ''
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  const handleChange = (e) => {
+    setValue(e.target.value);
   }
 
-  handleChange(e) {
-    this.setState({value: e.target.value});
-  }
-
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append('user[id]', this.props.userId);
-    formData.append(`user[relationship]`, this.state.value);
+    formData.append('user[id]', props.userId);
+    formData.append(`user[relationship]`, value);
 
-    this.props.updateUserAbout(formData).then(() => {
-      this.props.closeForm();
+    props.updateUserAbout(formData).then(() => {
+      props.closeForm();
     });
   }
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <select id="relationship-form" value={this.state.value} onChange={this.handleChange}>
-            <option value="">Status</option>
-            <option value="Single">Single</option>
-            <option value="In a relationship">In a relationship</option>
-            <option value="Engaged">Engaged</option>
-            <option value="Married">Married</option>
-            <option value="It's complicated">It's complicated</option>
-            <option value="Separated">Separated</option>
-            <option value="Divorced">Divorced</option>
-            <option value="Widowed">Widowed</option>
-          </select>
-          <div className='relationship-options'>
-            <button className="about-save" onClick={this.handleSubmit}>Save</button>
-            <button className="about-cancel" onClick={this.props.closeForm}>Cancel</button>
-          </div>
-        </form>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <select id="relationship-form" value={value} onChange={handleChange}>
+          <option value="">Status</option>
+          <option value="Single">Single</option>
+          <option value="In a relationship">In a relationship</option>
+          <option value="Engaged">Engaged</option>
+          <option value="Married">Married</option>
+          <option value="It's complicated">It's complicated</option>
+          <option value="Separated">Separated</option>
+          <option value="Divorced">Divorced</option>
+          <option value="Widowed">Widowed</option>
+        </select>
+        <div className='relationship-options'>
+          <button className="about-save" onClick={handleSubmit}>Save</button>
+          <button className="about-cancel" onClick={props.closeForm}>Cancel</button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 
