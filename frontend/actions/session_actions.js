245bb5e5bdd638075f02eeacdca56ea62e6f7b64
Kent Hiroi <kenthiroi@gmail.com>
2023-09-12 10:57:43 -0700

    laying out error msg components/actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -3,7 +3,8 @@ import * as APIUtil from '../util/session_api_util';
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
-export const CLEAR_ERRORS = "CLEAR_ERRORS";
+export const CLEAR_SESSION_ERRORS = "CLEAR_SESSION_ERRORS";
+export const CLEAR_SIGNUP_ERRORS = "CLEAR_SIGNUP_ERRORS";
 
 export const receiveCurrentUser = currentUser => ({
   type: RECEIVE_CURRENT_USER,
@@ -14,20 +15,24 @@ export const logoutCurrentUser = () => ({
   type: LOGOUT_CURRENT_USER,
 });
 
-export const receiveErrors = errors => ({
+export const receiveSessionErrors = errors => ({
   type: RECEIVE_SESSION_ERRORS,
   errors
 });
 
-export const clearErrors = () => ({
-  type: CLEAR_ERRORS
+export const clearSessionErrors = () => ({
+  type: CLEAR_SESSION_ERRORS,
 });
+
+export const clearSignupErrors = () => ({
+  type: CLEAR_SIGNUP_ERRORS,
+})
 
 export const signup = user => dispatch => (
   APIUtil.signup(user).then(user => (
     dispatch(receiveCurrentUser(user))
   ), err => (
-    dispatch(receiveErrors(err.responseJSON))
+    dispatch(receiveSessionErrors(err.responseJSON))
   ))
 );
 
@@ -35,12 +40,14 @@ export const login = user => dispatch => {
   return APIUtil.login(user).then(user => (
     dispatch(receiveCurrentUser(user))
   ), err => (
-    dispatch(receiveErrors(err.responseJSON))
+    dispatch(receiveSessionErrors(err.responseJSON))
   ))
 };
 
 export const logout = () => dispatch => (
   APIUtil.logout().then(() => (
     dispatch(logoutCurrentUser())
+  ), err => (
+    dispatch(receiveSessionErrors(err.responseJSON))
   ))
 );
--- frontend/components/error/errorMsg.jsx
+++ frontend/components/error/errorMsg.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
+import { clearUserErrors } from '../../actions/user_actions';
 
 const mSTP = (state) => {
   return {
-    errors: state.errors.user,
+    userErrors: state.errors.user,
   }
 }
 
-function ErrorMsg(errors){
-  
-  return(
-    <div id='global-error-bubble'>
-      {errors.map((error) => (
-        <div>{error}</div>
-      ))}
-    </div>
-  )
+const mDTP = (dispatch) => {
+  return {
+    clearUserErrors: () => dispatch(clearUserErrors)
+  }
 }
 
-export default connect(mSTP, null)(withRouter(ErrorMsg));
+function ErrorMsg(props){
+  
+  if(!!props.userErrors){
+    return(
+      <div id='global-error-bubble'>
+        {props.userErrors.map((error) => (
+          <div>{error}</div>
+        ))}
+      </div>
+    )
+  }
+  
+}
+
+export default connect(mSTP, mDTP)(withRouter(ErrorMsg));
